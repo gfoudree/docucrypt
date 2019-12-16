44e3b9d0766996088f816c3c7959b5b3d6e854f5
Grant Foudree <gfoudree@protonmail.com>
2019-12-15 21:26:36 -0600

    Updated UI form, using theme for all but the upload button. Messages dropdown working

--- website/static/main.js
+++ website/static/main.js
@@ -1,3 +1,37 @@
+$(document).ready(function () {
+    $('.ui.dropdown').dropdown(); // Initialize dropdown library for menu
+});
+
+function checkPassword() {
+    var pass = $('#passwordInputField').val();
+    if (pass.length < 8 && pass.length > 0) {
+        $('#passwordField').addClass('error');
+    }
+    else {
+        $('#passwordField').removeClass('error');
+    }
+}
+
+function displayMessage(message, title, bad) {
+    var html = '<div class="ui ';
+    if (bad) {
+        html += 'negative';
+    }
+    else {
+        html += 'positive';
+    }
+    html += ' message" id="message"> \
+        <i class="close icon" onclick="deleteMessage()"></i> \
+        <div class="header">' + title + '\n</div>\n<p>\n' + message + '\</p>\n</div>';
+
+    $(html).hide().fadeIn(500).insertAfter("#form");
+    $('.message .close').on('click', function() {
+        $(this)
+        .closest('.message')
+        .transition('fade');
+    });
+}
+
 function sendMessage(message, severity) {
     console.log(message);
 }
@@ -15,13 +49,14 @@ async function encrypt(data) {
     let iv = window.crypto.getRandomValues(new Uint8Array(16));
 
     let cipherText = await window.crypto.subtle.encrypt(
-        {"name": "AES-CBC",
-         iv
+        {
+            "name": "AES-CBC",
+            iv
         },
         aesKey,
         enc.encode(data)
     );
-    
+
     return cipherText;
 }
 
@@ -43,34 +78,35 @@ async function encryptData(data, password) {
     }
     else {
         // Derive AES key from password
-       let salt = window.crypto.getRandomValues(new Uint8Array(16));
-       salt = base64js.fromByteArray(salt);
-       scryptKey = await scrypt.async(
-           password,
-           salt,
-           16384,
-           8,
-           1,
-           32,
-           undefined,
-           300
-       );
-       key = await crypto.subtle.importKey(
-           "raw",
-           scryptKey,
-           {"name": "AES-CBC"},
-           true,
-           ["encrypt", "decrypt"]
-       );
+        let salt = window.crypto.getRandomValues(new Uint8Array(16));
+        salt = base64js.fromByteArray(salt);
+        scryptKey = await scrypt.async(
+            password,
+            salt,
+            16384,
+            8,
+            1,
+            32,
+            undefined,
+            300
+        );
+        key = await crypto.subtle.importKey(
+            "raw",
+            scryptKey,
+            { "name": "AES-CBC" },
+            true,
+            ["encrypt", "decrypt"]
+        );
 
-       console.log("key=" + base64js.fromByteArray(scryptKey));
-       console.log("iv=" + base64js.fromByteArray(iv));
-       console.log("salt=" +salt);
+        console.log("key=" + base64js.fromByteArray(scryptKey));
+        console.log("iv=" + base64js.fromByteArray(iv));
+        console.log("salt=" + salt);
     }
 
     let cipherText = window.crypto.subtle.encrypt(
-        {"name": "AES-CBC",
-         iv
+        {
+            "name": "AES-CBC",
+            iv
         },
         key,
         enc.encode(data)
@@ -78,7 +114,7 @@ async function encryptData(data, password) {
     let exportedKey = await crypto.subtle.exportKey("raw", key);
     exportedKey = new Uint8Array(exportedKey);
 
-    return {'IV': iv, 'CipherText': await cipherText, 'Key': exportedKey};
+    return { 'IV': iv, 'CipherText': await cipherText, 'Key': exportedKey };
 }
 
 function handleUpload() {
@@ -105,7 +141,7 @@ function handleUpload() {
                 return;
             }
 
-            encryptData(fReader.result, password).then(function(encryptedDocument) {
+            encryptData(fReader.result, password).then(function (encryptedDocument) {
                 console.log(encryptedDocument);
 
                 var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
@@ -113,25 +149,25 @@ function handleUpload() {
                 let fd = new FormData();
                 fd.append('data', base64js.fromByteArray(encBytes));
                 fd.append('IV', base64js.fromByteArray(encryptedDocument['IV']));
-                
+
                 $.ajax({
                     type: 'POST',
                     url: '/upload/',
                     data: fd,
-                    headers: {'X-CSRFToken': csrftoken},
+                    headers: { 'X-CSRFToken': csrftoken },
                     processData: false,
                     contentType: false,
-                    error: function(request, error) {
+                    error: function (request, error) {
                         sendMessage("Error sending encrypted file to server!", 'critical');
                     },
-                    success: function(data) {
-                        console.log("Successfully uploaded!");
+                    success: function (data) {
+                        displayMessage('', 'File upload success!', false);
                     }
                 });
             });
         }
         else {
-            encryptData(fReader.result, '').then(function(encryptedDocument) {
+            encryptData(fReader.result, '').then(function (encryptedDocument) {
                 console.log(encryptedDocument);
 
                 var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
@@ -139,19 +175,19 @@ function handleUpload() {
                 let fd = new FormData();
                 fd.append('data', base64js.fromByteArray(encBytes));
                 fd.append('IV', base64js.fromByteArray(encryptedDocument['IV']));
-                
+
                 $.ajax({
                     type: 'POST',
                     url: '/upload/',
                     data: fd,
-                    headers: {'X-CSRFToken': csrftoken},
+                    headers: { 'X-CSRFToken': csrftoken },
                     processData: false,
                     contentType: false,
-                    error: function(request, error) {
+                    error: function (request, error) {
                         sendMessage("Error sending encrypted file to server!", 'critical');
                     },
-                    success: function(data) {
-                        console.log("Successfully uploaded!");
+                    success: function (data) {
+                        displayMessage('', 'File upload success!', false);
                     }
                 });
             });
